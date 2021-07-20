ece8ac4fbf76406897526bdb1da2137b70c9f184
ryu2129 <ryu212920@gmail.com>
2021-07-21 00:05:36 +0900

    svg-loading

---  loading/index.js
+++  loading/index.js
@@ -1,25 +1,4 @@
-var bar = new ProgressBar.Line(splashText,{
-  strokeWidth: 0,
-  duration: 5000,
-  trailWidth: 0,
-  text: {
-    style: {
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: '0',
-      margin: '0',
-      Transform: 'translate(-50%, -50%)',
-      'font-size': '1.2rem',
-      color: '#fff',
-    },
-    autoStyleContainer: false
-  },
-  step: function(state,bar) {
-    bar.setText(Math.round(bar.value() * 100)+'%')
-  }
-});
-
-bar.animate(1.0, function(){
-  $("#splash").delay(500).fadeOut(800);
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');
+  $("#splash_logo").delay(1200).fadeOut('slow');
 });
