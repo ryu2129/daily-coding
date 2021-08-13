1de6039bb45bae956b7af9854a1c61b298a7aced
ryu2129 <ryu212920@gmail.com>
2021-08-13 10:00:31 +0900

    sample15

--- sample/index.js
+++ sample/index.js
@@ -5,4 +5,11 @@ jQuery(function($){
     $('.sample12').click(function(){
         $('.sample12, .sample12_inner').toggleClass('jq_trans')
     });
+    $('.sample14').click(function(e){
+        $('.sample14_circle').css({
+            left: e.pageX - $(this).offset().left - 15 + 'px',
+            top: e.pageY - $(this).offset().top - 15 + 'px',
+        });
+        return false;
+    })
 });
