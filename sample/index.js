727bd4a46e9fc9f1424a612d7d52e66163810e1f
ryu2129 <ryu212920@gmail.com>
2021-08-13 09:48:14 +0900

    sample12

--- sample/index.js
+++ sample/index.js
@@ -2,4 +2,7 @@ jQuery(function($){
     $('.sample11').click(function(){
         $(this).toggleClass('jq_trans');
     });
+    $('.sample12').click(function(){
+        $('.sample12, .sample12_inner').toggleClass('jq_trans')
+    });
 });
