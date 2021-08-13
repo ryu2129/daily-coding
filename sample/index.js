088730dd86e8a221ce3d536fc7b24dd5ed49ad8f
ryu2129 <ryu212920@gmail.com>
2021-08-13 10:29:57 +0900

    sample22

--- sample/index.js
+++ sample/index.js
@@ -2,14 +2,36 @@ jQuery(function($){
     $('.sample11').click(function(){
         $(this).toggleClass('jq_trans');
     });
+
     $('.sample12').click(function(){
         $('.sample12, .sample12_inner').toggleClass('jq_trans')
     });
+
     $('.sample14').click(function(e){
         $('.sample14_circle').css({
             left: e.pageX - $(this).offset().left - 15 + 'px',
             top: e.pageY - $(this).offset().top - 15 + 'px',
         });
         return false;
+    })
+
+    $('.sample15').click(function(){
+        $(this).toggleClass('rotate720')
+    })
+
+    $('.sample16').click(function(){
+        $(this).toggleClass('rotate720')
+    })
+
+    $('.sample17').click(function(){
+        $(this).toggleClass('rotateY720')
+    })
+
+    $('.sample18').click(function(){
+        $(this).toggleClass('rotateY720')
+    })
+
+    $('.sample22').click(function(){
+        $(this).toggleClass('mix')
     })
 });
